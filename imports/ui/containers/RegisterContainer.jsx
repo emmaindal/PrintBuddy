b63501ef8a96aefab2b111a375a05ea02c532905
Mikael Carlstein <Mikael.carlstein@wecode.se>
2017-04-05 17:00:51 +0200

    Added simple login and register connection with backend

--- imports/ui/containers/RegisterContainer.jsx
+++ imports/ui/containers/RegisterContainer.jsx
@@ -1,22 +1,49 @@
 import {Meteor} from 'meteor/meteor';
 import {createContainer} from 'meteor/react-meteor-data';
 import React from 'react';
+import {browserHistory} from 'react-router'
 
-import {RegisterComponent} from '../components/RegisterComponent';
-import Nav from '../components/Nav';
+import RegisterComponent from '../components/RegisterComponent';
+import {displayError} from '../helpers/errors';
 
 class Register extends React.Component {
 	constructor(props) {
         super(props);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onSubmit(formData){
+
+        // Todo fixa i register så man kan söka på sin address via gogole api och lägga till det
+        const position = {address: "fasfasf", lat: 1, lng: 2};
+
+        Accounts.createUser({
+            email: formData.email,
+            username:formData.username,
+            password:formData.password,
+            position:position
+        }, (err) => {
+            if (err) {
+                displayError("Error", err.reason)
+                console.log(err);
+            } else {
+                Meteor.call('sendVerificationLink', (error, response) => {
+                    if (error) {
+                        displayError("Error", error);
+                        console.log(error);
+                    } else {
+                        browserHistory.push('/');
+                    }
+                });
+            }
+        });
     }
 
 	render() {
-        const {items} = this.props;
 
 		return (
             <div>
-				<Nav/>
-                <RegisterComponent/>
+                <RegisterComponent submit={this.onSubmit}/>
 			</div>
 		);
 	}
@@ -33,3 +60,7 @@ const RegisterContainer = createContainer(() => {
 
 
 export default RegisterContainer;
+
+message = [
+    {userId:"1", text:"test"},{userId:"2", text:"test"},{userId:"123", text:"test231"},{userId:"2", text:"test3"},{userId:"1", text:"test4"},{userId:"2", text:"test5"}
+]
