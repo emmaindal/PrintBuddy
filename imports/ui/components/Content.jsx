a82278c9ab1bd7248b43dea330eca0617fc29481
Emma Indal <emma.indahl@gmail.com>
2017-04-05 16:32:53 +0200

    Första versionen av startsidan

---
+++ imports/ui/components/Content.jsx
@@ -0,0 +1,114 @@
+import React from 'react';
+
+
+export default class Content extends React.Component {
+
+    render() {
+        return(
+            <div className="row">
+                <div className="col s12 grid-example">
+                    <h1>PrintBuddy</h1>
+                    <h4>Under Development</h4>
+                    <h4>Test</h4>
+                    <a href="#second-content-row" className="btn-floating btn-large waves-effect waves-light">Scroll here</a>
+                </div>
+
+                <div className="col s6 grid-example" id="second-content-row">
+                    <h4>About PrintBuddy's</h4>
+                    <p>
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate
+                        velit esse cillum dolore eu fugiat nulla pariatur.
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                        qui officia deserunt mollit anim id est laborum."
+                    </p>
+                    <p>
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate
+                        velit esse cillum dolore eu fugiat nulla pariatur.
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                        qui officia deserunt mollit anim id est laborum."
+                    </p>
+                    <a href="#third-content-row" className="btn-floating btn-large waves-effect waves-light">Scroll here</a>
+
+                </div>
+                <div className="col s6 grid-example" id="second-content-row">
+                    <h4>About Requestor's</h4>
+                    <p>
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate
+                        velit esse cillum dolore eu fugiat nulla pariatur.
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                        qui officia deserunt mollit anim id est laborum."
+                    </p>
+                    <p>
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate
+                        velit esse cillum dolore eu fugiat nulla pariatur.
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                        qui officia deserunt mollit anim id est laborum."
+                    </p>
+                    <a href="#third-content-row" className="btn-floating btn-large waves-effect waves-light">Scroll here</a>
+
+                </div>
+                <div className="col s12 grid-example" id="third-content-row">
+                    <div className="col m4 s12">
+                        <div className="center promo promo-example">
+                            <i className="material-icons">grade</i>
+                            <p className="promo-caption">Heading</p>
+                            <p className="light center">"Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore et dolore magna
+                                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                Duis aute irure dolor in reprehenderit in voluptate
+                                velit esse cillum dolore eu fugiat nulla pariatur.
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                                qui officia deserunt mollit anim id est laborum."</p>
+                        </div>
+                    </div>
+                    <div className="col m4 s12">
+                        <div className="center promo promo-example">
+                            <i className="material-icons">group</i>
+                            <p className="promo-caption">Heading</p>
+                            <p className="light center">"Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore et dolore magna
+                                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                Duis aute irure dolor in reprehenderit in voluptate
+                                velit esse cillum dolore eu fugiat nulla pariatur.
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                                qui officia deserunt mollit anim id est laborum."</p>
+                        </div>
+
+                    </div>
+                    <div className="col m4 s12">
+                        <div className="center promo promo-example">
+                            <i className="material-icons">print</i>
+                            <p className="promo-caption">Heading</p>
+                            <p className="light center">"Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore et dolore magna
+                                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                Duis aute irure dolor in reprehenderit in voluptate
+                                velit esse cillum dolore eu fugiat nulla pariatur.
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                                qui officia deserunt mollit anim id est laborum."</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
--- imports/ui/containers/StartContainer.jsx
+++ imports/ui/containers/StartContainer.jsx
@@ -2,6 +2,10 @@ import {Meteor} from 'meteor/meteor';
 import {createContainer} from 'meteor/react-meteor-data';
 import React from 'react';
 
+import Nav from '../components/Nav';
+import Content from '../components/Content';
+import Footer from '../components/Footer';
+
 
 class Start extends React.Component {
 	constructor(props) {
@@ -9,10 +13,34 @@ class Start extends React.Component {
         this.state = {};
     }
 
+	componentDidMount(){
+		const $ = window.$;
+
+		$(document).ready(function () {
+		    $(function() {
+		      $('a[href*="#"]:not([href="#"])').click(function() {
+		        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		          var target = $(this.hash);
+		          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		          if (target.length) {
+		            $('html, body').animate({
+		              scrollTop: target.offset().top
+		            }, 1000);
+		            return false;
+		          }
+		        }
+		      });
+		    });
+		})
+
+	}
+
 	render() {
 		return (
 			<div>
-				<h1>Start Component</h1>
+				<Nav/>
+				<Content/>
+				<Footer/>
 			</div>
 		);
 	}
