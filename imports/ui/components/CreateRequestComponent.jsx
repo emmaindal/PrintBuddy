421a92155fac6a04d2d97dc8906a4aa8cc0666c4
Alexloof <alex.loofet@gmail.com>
2017-04-10 16:25:14 +0200

    Added new fields to request api and the request form

--- imports/ui/components/CreateRequestComponent.jsx
+++ imports/ui/components/CreateRequestComponent.jsx
@@ -15,6 +15,7 @@ class CreateRequest extends React.Component {
             format: 'yyyy-mm-dd',
             today: "Today"
         });
+        $('select').material_select();
     }
 
     changeRange() {
@@ -33,7 +34,11 @@ class CreateRequest extends React.Component {
                 needColor: this.refs.needColor.checked,
                 radius: parseInt(this.state.range),
                 lastDate: new Date(this.refs.lastDate.value),
-                reward: parseInt(this.refs.reward.value)
+                reward: parseInt(this.refs.reward.value),
+                pages: parseInt(this.refs.pages.value),
+                copies: parseInt(this.refs.copies.value),
+                currency: this.refs.currency.value,
+                title: this.refs.title.value.trim()
             }
             this.props.submit(request)
         }
@@ -41,12 +46,6 @@ class CreateRequest extends React.Component {
     render() {
         return (
             <div className="row">
-            <div className="row" >
-                            <div className="input-field col s8 offset-s2" >
-                                <input id="input-text" type="text" data-length="10"/>
-                                <label htmlFor="input-text">Job title</label>
-                            </div>
-                        </div>
                 <div className="col s12 m8 l6 xl4 offset-m2 offset-l3 offset-xl4 create-request">
                     <h3 className="center-align">Create Request</h3>
                     <form onSubmit={this.onSubmit.bind(this)}>
@@ -63,9 +62,9 @@ class CreateRequest extends React.Component {
                             <div className="col s2">
                             </div>
                         </div>
-                        <div className="row" >
+                        <div className="row margin-bottom-after" >
                             <div className="input-field col s8 offset-s2" >
-                                <input id="input-text" type="text" data-length="10"/>
+                                <input ref="title" className="validate" id="input-text" type="text" maxLength="40" required/>
                                 <label htmlFor="input-text">Job title</label>
                             </div>
                         </div>
@@ -77,13 +76,46 @@ class CreateRequest extends React.Component {
                                 Color
                             </label>
                         </div>
-                        <div className="switch center-align margin-bottom-after">
+                        <div className="switch center-align">
                             <label className="second-switch">
                                 No Delivery
                                 <input type="checkbox" ref="delivery" />
                                 <span className="lever"></span>
                                 Home Delivery
                             </label>
+                        </div>
+                        <div className="row" >
+                            <div className="input-field inline col s8 offset-s2" >
+                                <input id="number" type="number" ref="pages" className="validate" required />
+                                <label htmlFor="number">Nr of Pages</label>
+                            </div>
+                        </div>
+                        <div className="row margin-bottom-after" >
+                            <div className="input-field inline col s8 offset-s2" >
+                                <input id="number" type="number" ref="copies" className="validate" required />
+                                <label htmlFor="number">Nr of Copies</label>
+                            </div>
+                        </div>
+                        <div className="row margin-bottom-after" >
+                            <div className="input-field col s4 offset-s2" >
+                                <input id="number" type="number" ref="reward" className="validate" required/>
+                                <label htmlFor="number">Reward</label>
+                            </div>
+                            <div className="input-field col s4">
+                                <select ref="currency">
+                                    <option defaultValue="sek">SEK</option>
+                                    <option value="eur">EUR</option>
+                                    <option value="usd">USD</option>
+                                </select>
+                                <label>Currency</label>
+                            </div>
+                        </div>
+                        <p className="center-align">Set a last date for pick-up/delivery</p>
+                        <div className="row margin-bottom-after">
+                            <div className="col s8 offset-s2 input-field inline">
+                                <input id="date" type="date" ref="lastDate" className={this.state.dateClassName} />
+                                <label htmlFor="date">Date</label>
+                            </div>
                         </div>
                         <p className="center-align">Set a max distance</p>
                         <div className="row margin-bottom-after">
@@ -92,26 +124,11 @@ class CreateRequest extends React.Component {
                                 <input onChange={this.changeRange.bind(this)} ref="range" type="range" id="test5" min="1" max="10000" />
                             </p>
                         </div>
-                        <p className="center-align">Set a last date for pick-up/delivery</p>
-                        <div className="row">
-                            <div className="col s8 offset-s2 input-field inline">
-                                <input id="date" type="date" ref="lastDate" className={this.state.dateClassName} />
-                                <label htmlFor="date">Date</label>
-                            </div>
-                        </div>
-                        <div className="row margin-bottom-after" >
-                            <div className="input-field inline col s8 offset-s2" >
-                                <input id="number" type="number" ref="reward" className="validate" required />
-                                <label htmlFor="number">Offer</label>
-                            </div>
-
-                        </div>
                         <div className="center-align">
                             <button className="btn-large waves-effect waves-light margin-bottom-after" type="submit" name="action">Submit</button>
                         </div>
                     </form>
                 </div>
-
             </div>
         );
     }
