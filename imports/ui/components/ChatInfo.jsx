aab376e97fdeb7c17595e9bc1268f3d4e1590433
Alexloof <alex.loofet@gmail.com>
2017-04-10 13:48:26 +0200

    Added chat info and download button + modal and tweaks for mobile etc

---
+++ imports/ui/components/ChatInfo.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+class ChatInfo extends React.Component {
+    render() {
+        return (
+            <div>
+                <ul className="collection with-header chat-info-wrapper">
+                    <li className="collection-header"><h4>Job info</h4></li>
+                    <li className="collection-item"><span>Namn:</span> Alexander Lööf </li>
+                    <li className="collection-item"><span>Adress:</span> Webersgade 7. St th 2100 Köpenhamn S</li>
+                    <li className="collection-item"><span>Color:</span> No </li>
+                    <li className="collection-item"><span>Delivery:</span> Yes </li>
+                    <li className="collection-item"><span>Last Date:</span> 2017-04-20</li>
+                </ul>
+            </div>
+
+        );
+    }
+}
+
+export default ChatInfo;
