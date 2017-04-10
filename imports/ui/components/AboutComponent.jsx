eca3b90e99c9cef9a0308e04a44439c243f93abd
Emma Indal <emma.indahl@gmail.com>
2017-04-10 11:31:21 +0200

    små ändringar, scss-fil tillagd för aboutcomponent bland annat

--- imports/ui/components/AboutComponent.jsx
+++ imports/ui/components/AboutComponent.jsx
@@ -11,21 +11,28 @@ export default class AboutComponent extends React.Component {
                             <img src="https://static.pexels.com/photos/273238/pexels-photo-273238.jpeg" />
                             <div className="caption center-align">
                                 <h3>This is our big Tagline!</h3>
-                                <h5 className="light grey-text text-lighten-3">Here's our small slogan..</h5>
+                                <h5 className="light black-text text-lighten-3">Here's our small slogan..</h5>
                             </div>
                         </li>
                         <li>
                             <img src="https://static.pexels.com/photos/33488/navigation-car-drive-road.jpg" />
                             <div className="caption left-align">
                                 <h3>Left Aligned Caption!</h3>
-                                <h5 className="light grey-text text-lighten-3">Here's our small slogan..</h5>
+                                <h5 className="light black-text text-lighten-3">Here's our small slogan..</h5>
                             </div>
                         </li>
                         <li>
                             <img src="https://static.pexels.com/photos/52608/pexels-photo-52608.jpeg" />
                             <div className="caption right-align">
                                 <h3>Right Aligned Caption!</h3>
-                                <h5 className="light grey-text text-lighten-3">Here's our small slogan..</h5>
+                                <h5 className="light black-text text-lighten-3">Here's our small slogan..</h5>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="https://static.pexels.com/photos/48734/pexels-photo-48734.jpeg" />
+                            <div className="caption center-align">
+                                <h3>This is our big Tagline!</h3>
+                                <h5 className="light black-text text-lighten-3">Here's our small slogan..</h5>
                             </div>
                         </li>
                     </ul>
