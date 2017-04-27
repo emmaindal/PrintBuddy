1384931a2d391b1e3c60f97eadbb2aa6cb277213
Emma Indal <emma.indahl@gmail.com>
2017-04-27 15:13:42 +0200

    start, lagt till bilder etc

--- imports/ui/components/Content.jsx
+++ imports/ui/components/Content.jsx
@@ -10,20 +10,21 @@ export default class Content extends React.Component {
     popLoginModal() {
         $('#register-modal').closeModal();
         $('#login-modal').openModal({
-            inDuration: 100, 
-            outDuration: 100,  
+            inDuration: 100,
+            outDuration: 100,
         });
     }
     popRegisterModal() {
         $('#login-modal').closeModal();
         $('#register-modal').openModal({
-            inDuration: 100, 
-            outDuration: 100,      
+            inDuration: 100,
+            outDuration: 100,
         });
     }
     render() {
         return (
             <div className="start-page">
+
                 <div className="row">
                     <div className="col s12 m12 l12 grid-example content-first" id="first-row">
                         <h1>Welcome to PrintBuddy</h1>
@@ -36,19 +37,21 @@ export default class Content extends React.Component {
                     <div className="row two">
                         <div className="col m6 s12 grid-example content-second left" id="second-row">
                             <div className="text-wrapper left">
-                                <h4>Heading</h4>
+                                <h4>Get your document delivered</h4>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                                    <div className="img-wrapper hide-on-large-only">
-                                        <img src="https://static.pexels.com/photos/7075/people-office-group-team.jpg"/>
-                                    </div>
-                            </div>
-                            <div className="arrow bounce">
-                                <a className="fa fa-arrow-down fa-2x" href="#third-row"></a>
+
+                                <div className="img-wrapper hide-on-large-only">
+                                    <img src="https://static.pexels.com/photos/6508/nature-laptop-outside-macbook.jpg"/>
+                                </div>
+
+                                <div className="arrow bounce">
+                                    <a className="fa fa-arrow-down fa-2x" href="#third-row"></a>
+                                </div>
                             </div>
                         </div>
                         <div className="col m6 s12 grid-example content-second right hide-on-med-and-down">
                             <div className="img-wrapper right">
-                                <img src="https://static.pexels.com/photos/7075/people-office-group-team.jpg" />
+                                <img src="https://static.pexels.com/photos/33488/navigation-car-drive-road.jpg"/>
                             </div>
                         </div>
                     </div>
@@ -60,7 +63,7 @@ export default class Content extends React.Component {
                         </div>
                         <div className="col m6 s12 grid-example content-second right">
                             <div className="text-wrapper right">
-                                <h4>Heading</h4>
+                                <h4>All over the world</h4>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                             </div>
                             <div className="arrow bounce">
@@ -71,19 +74,19 @@ export default class Content extends React.Component {
                     <div className="row two">
                         <div className="col m6 s12 grid-example content-second left" id="fourth-row">
                             <div className="text-wrapper left">
-                                <h4>Heading</h4>
+                                <h4>Responsive design</h4>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                             </div>
-
+                            <div className="arrow bounce">
+                                <a className="fa fa-arrow-down fa-2x" href="#fifth-row"></a>
+                            </div>
                         </div>
                         <div className="col m6 s12 grid-example content-second right hide-on-med-and-down">
                             <div className="img-wrapper right" >
-                                <img src="https://static.pexels.com/photos/33488/navigation-car-drive-road.jpg"/>
+                                <img src="https://static.pexels.com/photos/6508/nature-laptop-outside-macbook.jpg" />
                             </div>
                         </div>
-                        <div className="arrow bounce">
-                            <a className="fa fa-arrow-down fa-2x" href="#fifth-row"></a>
-                        </div>
+
                     </div>
                 </div>
                 <div id="login-modal" className="modal login-modal">
