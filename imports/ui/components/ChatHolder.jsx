ddae52fa5c506c7a99e08f8e8e79e3da5c3b2036
Alexloof <alex.loofet@gmail.com>
2017-04-29 17:43:08 +0200

    Chat modal info very nice

--- imports/ui/components/ChatHolder.jsx
+++ imports/ui/components/ChatHolder.jsx
@@ -40,10 +40,12 @@ class ChatHolder extends React.Component {
             <div className="chat-holder">
                 <div className="chat-header">
                     <a onClick={this.showChatInfo.bind(this)}><i className="info-icon material-icons hide-on-large-only">info_outline</i></a>
-                    <h6 className="chat-title">Chat</h6>
+                    <h6 className="chat-title center-align">Chat</h6>
+                    {!canCancel ? 
                     <a onClick={this.props.handleDownload} className="waves-effect waves-light btn mobile-download-btn hide-on-large-only">Download</a>
+                    : null}
                 </div>
-                <div id="chatbox" className="chat chat-message" style={{minHeight: this.chatHeight(), maxHeight: this.chatHeight()}} >
+                <div id="chatbox" className="chat chat-message">
                     <ChatMessageList userId={this.props.userId} chat={this.props.chat} />
                 </div>
                 <div className="chat-form chat">
@@ -52,11 +54,9 @@ class ChatHolder extends React.Component {
 
                 {/*JobInfo Modal that open on click in mobile view*/}
                 <div id="modal1" className="modal">
-                    <div className="modal-content">
+                    <div className="modal-content" style={{padding:0}}>
+                        <i className="material-icons modal-close modal-action cancel-icon">clear</i>
                         <ChatInfo request={this.props.request} canCancel={canCancel}/>
-                    </div>
-                    <div className="modal-footer">
-                        <a className="modal-action modal-close waves-effect waves-green btn-flat">Close</a>
                     </div>
                 </div>
                 <div id="cancelModal" className="modal">
