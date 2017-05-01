9be6a76d08a34d9dd56aa1a8c5493b2f399c8c26
Mikael Carlstein <Mikael.carlstein@wecode.se>
2017-05-01 18:53:28 +0200

    added factoy

---
+++ imports/api/request/server/request.test.js
@@ -0,0 +1,20 @@
+import { Meteor } from 'meteor/meteor';
+//import { Factory } from 'fac';
+//import { PublicationCollector } from 'meteor/publication-collector';
+import expect from 'expect'
+import { Random } from 'meteor/random';
+import { _ } from 'meteor/underscore';
+
+import { Request } from '../request.js';
+
+
+
+describe('request', () => {
+    beforeEach(function () {
+        // Stuff to do for each it() function.
+    });
+
+    it('adadsasd', function () {
+        //expect().toNotThrow();
+    })
+});
