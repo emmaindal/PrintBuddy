752d451564fc076c601c618254b95e8d010296d7
Alexloof <alex.loofet@gmail.com>
2017-05-06 18:10:15 +0200

    Our team design!

--- imports/ui/components/AboutComponent.jsx
+++ imports/ui/components/AboutComponent.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 class AboutComponent extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { emailSuccess: ""};
+        this.state = { emailSuccess: "" };
     }
-    onSubmitEmail(e){
+    onSubmitEmail(e) {
         e.preventDefault();
         const message = {
             email: this.refs.email.value,
@@ -14,10 +14,10 @@ class AboutComponent extends React.Component {
         }
 
         this.props.onSubmitEmail(message);
-        this.setState({emailSuccess: "Sending success"});
+        this.setState({ emailSuccess: "Sending success" });
     }
     render() {
-        return(
+        return (
             <div id="about-content">
                 <div className="row about-row">
 
@@ -37,12 +37,12 @@ class AboutComponent extends React.Component {
                                         Our goal with this project is to make it easier for you to get any
                                         document printed out in your neighborhood.
                                     </p>
-                                    <br/>
+                                    <br />
                                     <p>We know how hard it is to really need, for example a CV, printed out.
                                         You have no printer at home, you need to go to a library or something
                                         and it’s too late because the library closes at 6 pm.
                                     </p>
-                                    <br/>
+                                    <br />
                                     <p>
                                         Therefore, we, as a team, works for a easy way to make your days more comfortable.
                                         Just a click away and you get the ability to have your really important documents in
@@ -56,144 +56,92 @@ class AboutComponent extends React.Component {
                         <div className="col s12 m12 l12 grid-example about-content third">
                             <section className="section-about-content third">
                                 <h4 className="our-team-heading">OUR TEAM</h4>
-                                <div className="col s12 m3 l3">
-                                    <div className="card card-avatar">
-                                        <div className="waves-effect waves-block waves-light">
-                                            <img className="profile-picture" src="https://static.pexels.com/photos/91227/pexels-photo-91227.jpeg" alt="profile-picture"/>
-                                        </div>
-                                        <div className="card-content">
-                                            <span className="card-title activator grey-text text-darken-4">Simon<i className="material-icons right">more_vert</i></span>
-                                                <span>
-                                                <h6 className="team-position">Developer</h6>
-                                                <div className="contact-icons">
-                                                    <a href="mailto:simon.holm@se.issworld.com"><i className="fa fa-envelope" aria-hidden="true"></i></a>
-                                                    <a href="https://se.linkedin.com/in/simon-holm-50769aa6"><i className="fa fa-linkedin-square" aria-hidden="true"></i></a>
-                                                </div>
-                                            </span>
-                                        </div>
-                                        <div className="card-reveal">
-                                            <span className="card-title grey-text text-darken-4"><h5>Simon Holm</h5><i className="material-icons right">close</i></span>
-                                            <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                                        </div>
+                                <div className="col s12 m6 l3">
+                                    <figure className="effect-lexi z-depth-2">
+                                        <img src="http://tympanus.net/Development/HoverEffectIdeas/img/12.jpg" alt="img12"></img>
+                                        <figcaption>
+                                            <h2>Simon <span>Holm</span></h2>
+                                            <p>Each and every friend is special. Lexi won't hide a single cookie.</p>
+                                            <a href="#">View more</a>
+                                        </figcaption>
+                                    </figure>
                                 </div>
+                                <div className="col s12 m6 l3">
+                                    <figure className="effect-lexi z-depth-2">
+                                        <img src="http://tympanus.net/Development/HoverEffectIdeas/img/12.jpg" alt="img12"></img>
+                                        <figcaption>
+                                            <h2>Emma <span>Indahl</span></h2>
+                                            <p>Each and every friend is special. Lexi won't hide a single cookie.</p>
+                                            <a href="#">View more</a>
+                                        </figcaption>
+                                    </figure>
                                 </div>
-                                <div className="col s12 m3 l3">
-                                    <div className="card card-avatar">
-                                        <div className="waves-effect waves-block waves-light">
-                                            <img className="profile-picture" src="https://static.pexels.com/photos/91227/pexels-photo-91227.jpeg" alt="profile-picture"/>
-                                        </div>
-                                        <div className="card-content">
-                                            <span className="card-title activator grey-text text-darken-4">Emma<i className="material-icons right">more_vert</i></span>
-                                            <span>
-                                            <h6 className="team-position">Developer/Designer</h6>
-                                                <div className="contact-icons">
-                                                    <a href="mailto:emma.indahl@gmail.com"><i className="fa fa-envelope" aria-hidden="true"></i></a>
-                                                    <a href="https://www.linkedin.com/in/emma-indal/"><i className="fa fa-linkedin-square" aria-hidden="true"></i></a>
-                                                </div>
-                                            </span>
-
-                                        </div>
-                                        <div className="card-reveal">
-                                            <span className="card-title grey-text text-darken-4"><h5>Emma Indal</h5><i className="material-icons right">close</i></span>
-                                            <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                                        </div>
-                                    </div>
-
+                                <div className="col s12 m6 l3">
+                                    <figure className="effect-lexi z-depth-2">
+                                        <img src="http://tympanus.net/Development/HoverEffectIdeas/img/12.jpg" alt="img12"></img>
+                                        <figcaption>
+                                            <h2>Mikael <span>Carlstein</span></h2>
+                                            <p>Each and every friend is special. Lexi won't hide a single cookie.</p>
+                                            <a href="#">View more</a>
+                                        </figcaption>
+                                    </figure>
                                 </div>
-                                <div className="col s12 m3 l3">
-                                    <div className="card card-avatar">
-                                        <div className="waves-effect waves-block waves-light">
-                                            <img className="profile-picture" src="https://static.pexels.com/photos/91227/pexels-photo-91227.jpeg" alt="profile-picture"/>
-                                        </div>
-                                        <div className="card-content">
-                                            <span className="card-title activator grey-text text-darken-4">Mikael<i className="material-icons right">more_vert</i></span>
-                                            <span>
-                                                <h6 className="team-position">Chief Programmer</h6>
-                                                <div className="contact-icons">
-                                                    <a href="mailto:mikael.carlstein@gmail.com"><i className="fa fa-envelope" aria-hidden="true"></i></a>
-                                                    <a href="https://se.linkedin.com/in/mikael-carlstein-79904249"><i className="fa fa-linkedin-square" aria-hidden="true"></i></a>
-                                                </div>
-                                            </span>
-
-                                        </div>
-                                        <div className="card-reveal">
-                                            <span className="card-title grey-text text-darken-4"><h5>Mikael Carlstein</h5><i className="material-icons right">close</i></span>
-                                            <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                                        </div>
-                                    </div>
-
+                                <div className="col s12 m6 l3">
+                                    <figure className="effect-lexi z-depth-2">
+                                        <img src="http://tympanus.net/Development/HoverEffectIdeas/img/12.jpg" alt="img12"></img>
+                                        <figcaption>
+                                            <h2>Alexander <span>Lööf</span></h2>
+                                            <p>Each and every friend is special. Lexi won't hide a single cookie.</p>
+                                            <a href="#">View more</a>
+                                        </figcaption>
+                                    </figure>
                                 </div>
-                                <div className="col s12 m3 l3">
-                                    <div className="card card-avatar">
-                                        <div className="card-image waves-effect waves-block waves-light">
-                                            <img className="profile-picture" src="https://static.pexels.com/photos/91227/pexels-photo-91227.jpeg" alt="profile-picture"/>
-                                        </div>
-                                        <div className="card-content">
-                                            <span className="card-title activator grey-text text-darken-4">Alexander<i className="material-icons right">more_vert</i></span>
-                                            <span>
-                                                <h6 className="team-position">Developer/Designer</h6>
-                                                <div className="contact-icons">
-                                                    <a href="mailto:alex.loofet@gmail.com"><i className="fa fa-envelope" aria-hidden="true"></i></a>
-                                                    <a href="https://se.linkedin.com/in/alexander-l%C3%B6%C3%B6f-854436115"><i className="fa fa-linkedin-square" aria-hidden="true"></i></a>
-                                                </div>
-                                            </span>
-                                        </div>
-                                        <div className="card-reveal">
-                                            <span className="card-title grey-text text-darken-4"><h5>Alexander Lööf</h5><i className="material-icons right">close</i></span>
-                                            <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                                        </div>
-
-                                    </div>
-
-                                </div>
-
-
                             </section>
-                        </div>
                     </div>
-                    <div className="row about-row">
-                        <div className="col s12 m12 l12 grid-example about content fourth">
-                            <section className="col s12 m6 l6 section-about-content fourth one">
-                                <h4>Say Hello!</h4>
-                                <p>We are happy to answer any and all of your
+                </div>
+                <div className="row about-row">
+                    <div className="col s12 m12 l12 grid-example about content fourth">
+                        <section className="col s12 m6 l6 section-about-content fourth one">
+                            <h4>Say Hello!</h4>
+                            <p>We are happy to answer any and all of your
                                     questions concerning our service.
                                     Just fill in the form to the right and we will answer
                                     as soon as possible. Have a great day!
                                 </p>
-                                <ul>
-                                    <li><a  href="mailto:info@printbuddy.se"><i className="material-icons">email</i> info@printbuddy.se</a></li>
-                                </ul>
-                            </section>
-                            <section className="col s12 m6 l6 section-about-content fourth two">
-                                <div className="formContainer">
-                                    <form action="" onSubmit={this.onSubmitEmail.bind(this)}>
-                                        <div className="row">
-                                            <div className="input-field col s12">
-                                                <input id="name" type="text" className="validate" required ref="name"/>
-                                                <label htmlFor="name">Name:</label>
-                                            </div>
+                            <ul>
+                                <li><a href="mailto:info@printbuddy.se"><i className="material-icons">email</i> info@printbuddy.se</a></li>
+                            </ul>
+                        </section>
+                        <section className="col s12 m6 l6 section-about-content fourth two">
+                            <div className="formContainer">
+                                <form action="" onSubmit={this.onSubmitEmail.bind(this)}>
+                                    <div className="row">
+                                        <div className="input-field col s12">
+                                            <input id="name" type="text" className="validate" required ref="name" />
+                                            <label htmlFor="name">Name:</label>
                                         </div>
-                                        <div className="row">
-                                            <div className="input-field col s12">
-                                                <input id="email" type="email" className="validate" required ref="email"/>
-                                                <label htmlFor="email">Email:</label>
-                                            </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="input-field col s12">
+                                            <input id="email" type="email" className="validate" required ref="email" />
+                                            <label htmlFor="email">Email:</label>
                                         </div>
-                                        <div className="row">
-                                            <div className="input-field col s12">
-                                                <textarea id="textarea1" className="materialize-textarea" required ref="text"/>
-                                                <label htmlFor="textarea1">Write your message:</label>
-                                            </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="input-field col s12">
+                                            <textarea id="textarea1" className="materialize-textarea" required ref="text" />
+                                            <label htmlFor="textarea1">Write your message:</label>
                                         </div>
-                                        <button className="waves-effect waves-light btn" type="submit">Send</button>
-                                        <p id="about-formContainer-success">{this.state.emailSuccess}</p>
-                                    </form>
-                                </div>
-                            </section>
-                        </div>
+                                    </div>
+                                    <button className="waves-effect waves-light btn" type="submit">Send</button>
+                                    <p id="about-formContainer-success">{this.state.emailSuccess}</p>
+                                </form>
+                            </div>
+                        </section>
                     </div>
                 </div>
             </div>
+            </div >
         )
     }
 }
